dfdd303d9e9d3a00649d92e42fa642411e9bd23c
tpx_samuel <samuel@toutenpixel.fr>
2020-12-28 16:32:20 +0100

    CRUD

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const port = 3000;
 
 const routerFile = require('./router');
 
-// app.get('/', (req, res) => {
-//     res.send('Boussus Samuel');
-// })
-
-// app.use((request, response, next) => {
-//     console.log()
-//     next()
-// })
-
 app.use('/', routerFile);
 
 app.listen(port);
--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 router.use(function timeLog(req, res, next) {
     let currentTime = Date.now();
@@ -12,5 +13,27 @@ router.use(function timeLog(req, res, next) {
 router.get('/', (req, res) => {
     res.send('Boussus Samuel');
 })
+
+router.route('/orders')
+    .get((req, res) => {
+        res.send('Voici la liste des orders')
+    })
+    .post((req, res) => {
+        res.send(`Order d'id ${uuidv4()} créée`)
+    })
+
+router.route('/orders/[0-9]+/')
+    .get((req, res) => {
+        let id = req.originalUrl.split('/')[2];
+        res.send(`Voici l'order d'id ${id}`);
+    })
+    .put((req, res) => {
+        let id = req.originalUrl.split('/')[2];
+        res.send(`Order d'id ${id} mis à jour`);
+    })
+    .delete((req, res) => {
+        let id = req.originalUrl.split('/')[2];
+        res.send(`Order d'id ${id} supprimée`);
+    })
 
 module.exports = router;
